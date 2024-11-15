8ba18d4bb6ee06528d0b8bc31e136b89330e17ab
Lucas MacQuarrie <MacLucas07@hotmail.com>
2024-11-15 22:27:53 +0100

    Added PostQuery include Votes and Comments

--- FMSEvaluering.Application/Queries/QueryDto/PostDto.cs
+++ FMSEvaluering.Application/Queries/QueryDto/PostDto.cs
@@ -5,6 +5,9 @@ public record PostDto()
     public int Id { get; set; }
     public string Description { get; set; }
     public string Solution { get; set; }
+    public IEnumerable<VoteDto> VoteDto { get; set; }
+    public IEnumerable<CommentDto> CommentDto { get; set; }
+
     // IEnumerable VoteDto
     // IEnumerable CommentDto
     // StudentId
--- FMSEvaluering.Infrastructure/Queries/PostQuery.cs
+++ FMSEvaluering.Infrastructure/Queries/PostQuery.cs
@@ -14,14 +14,34 @@ public class PostQuery : IPostQuery
     }
     async Task<PostDto> IPostQuery.GetPostAsync(int postId)
     {
-        var post = await _db.Posts.AsNoTracking()
+        //var post = await _db.Posts.AsNoTracking()
+        //    .SingleAsync(p => p.Id == postId);
+
+        //return new PostDto
+        //{
+        //    Id = post.Id,
+        //    Description = post.Description,
+        //    Solution = post.Solution
+        //};
+
+        var post = await _db.Posts.AsNoTracking().Include(p => p.Comments).Include(p => p.Votes)
             .SingleAsync(p => p.Id == postId);
 
         return new PostDto
         {
             Id = post.Id,
             Description = post.Description,
-            Solution = post.Solution
+            Solution = post.Solution,
+            VoteDto = post.Votes.Select(v => new VoteDto
+            {
+                Id = v.Id,
+                VoteType = v.VoteType
+            }),
+            CommentDto = post.Comments.Select(c => new CommentDto
+            {
+                Id = c.Id,
+                Text = c.Text
+            })
         };
     }
 }
