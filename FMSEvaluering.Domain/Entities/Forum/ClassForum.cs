5c2911c4e191537fb4cb935d61ac4db47fa848e0
ZmolZito <maclucas07@hotmail.com>
2024-11-25 00:07:56 +0100

    Forum domain tilføjet til Evaluering

---
+++ FMSEvaluering.Domain/Entities/Forum/ClassForum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMSEvaluering.Domain.Entities.Forum
+{
+    public class ClassForum : Forum
+    {
+        public int ClassId { get; set; }
+    }
+}
---
+++ FMSEvaluering.Domain/Entities/Forum/PublicForum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMSEvaluering.Domain.Entities.Forum
+{
+    public class PublicForum : Forum
+    {
+        internal PublicForum(string name)
+        {
+            Name = name;
+        }
+    }
+}
