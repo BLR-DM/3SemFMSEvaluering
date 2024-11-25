2166314b20d547d0a3418627994df5646f89d7c8
ZmolZito <maclucas07@hotmail.com>
2024-11-25 21:01:31 +0100

    tilføjet Ctor til ClassForum

--- FMSEvaluering.Domain/Entities/Forum/ClassForum.cs
+++ FMSEvaluering.Domain/Entities/Forum/ClassForum.cs
@@ -9,5 +9,11 @@ namespace FMSEvaluering.Domain.Entities.Forum
     public class ClassForum : Forum
     {
         public int ClassId { get; set; }
+
+        internal ClassForum(string name, int classId)
+        {
+            Name = name;
+            ClassId = classId;
+        }
     }
 }
