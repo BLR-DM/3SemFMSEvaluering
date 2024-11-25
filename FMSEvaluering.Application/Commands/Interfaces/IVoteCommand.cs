db681e08d90b0daa9b8371b1c6be5718a7ae78d4
ZmolZito <maclucas07@hotmail.com>
2024-11-25 21:03:00 +0100

    IVoteCommand slettet

--- FMSEvaluering.Application/Commands/Interfaces/IVoteCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using FMSEvaluering.Application.Commands.CommandDto.VoteDto;
-
-namespace FMSEvaluering.Application.Commands.Interfaces
-{
-    public interface IVoteCommand
-    {
-        void CreateVote(CreateVoteDto vote);
-    }
-}
