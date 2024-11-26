37b0bd1d49727a8c2fdda081c130e1a7d68cf368
ZmolZito <maclucas07@hotmail.com>
2024-11-27 00:20:16 +0100

    Rettet exitslip entity

--- FMSExitSlip.Domain/Entities/ExitSlip.cs
+++ FMSExitSlip.Domain/Entities/ExitSlip.cs
@@ -10,6 +10,8 @@ namespace FMSExitSlip.Domain.Entities
     {
         private readonly List<Question> _questions = [];
         public string Title { get; protected set; }
+        public int MaxQuestions { get; protected set; }
+        public bool IsPublished { get; protected set; } = false;
         public int LectureId { get; protected set; }
 
         public ICollection<Question> Questions => _questions;
