8468b9baddbc7b6920537780ad8156d35634bb87
skovbov <skovb@DESKTOP-TKGR0FM>
2024-11-27 01:35:16 +0100

    fixed minor bugs

--- FMSExitSlip.Application/Commands/ExitSlipCommand.cs
+++ FMSExitSlip.Application/Commands/ExitSlipCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FMSExitSlip.Application.Commands.CommandDto.ExitSlipDto;
 using FMSExitSlip.Application.Commands.Interfaces;
 using FMSExitSlip.Application.Helpers;
 using FMSExitSlip.Application.Repositories;
@@ -22,28 +23,30 @@ namespace FMSExitSlip.Application.Commands
             _unitOfWork = unitOfWork;
             _exitSlipRepository = exitSlipRepository;
         }
-        Task IExitSlipCommand.CreateQuestion(CreateExitSlipDto exitSlipDto)
+
+        async Task IExitSlipCommand.CreateQuestion(CreateExitSlipDto exitSlipDto)
         {
-			try
+            try
             {
-                _unitOfWork.BeginTransaction();
+                await _unitOfWork.BeginTransaction();
 
                 // Load
                 //var exitSlip = _exitSlipRepository
 
 
             }
-			catch (Exception)
-			{
+            catch (Exception)
+            {
 
-				throw;
-			}
-   
+                throw;
+            }
+        }
+
         async Task IExitSlipCommand.CreateExitSlipAsync(string title, int maxQuestions, string appUserId, int lectureId)
         {
             try
-            { 
-                await _unitOfwork.BeginTransaction();
+            {
+                await _unitOfWork.BeginTransaction();
 
                 // Load
                 var otherExitSlips = await _exitSlipRepository.GetExitSlipsAsync();
@@ -53,11 +56,11 @@ namespace FMSExitSlip.Application.Commands
                 await _exitSlipRepository.AddExitSlipAsync(exitSlip);
 
                 // Save
-                await _unitOfwork.Commit();
+                await _unitOfWork.Commit();
             }
             catch (Exception)
             {
-                await _unitOfwork.Rollback();
+                await _unitOfWork.Rollback();
                 throw;
             }
         }
