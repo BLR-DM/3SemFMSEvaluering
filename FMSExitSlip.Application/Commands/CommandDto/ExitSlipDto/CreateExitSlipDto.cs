36211e4d618457f915af70c5f01b38042baedd6b
skovbov <skovb@DESKTOP-TKGR0FM>
2024-11-27 01:25:54 +0100

    Added Create Question

---
+++ FMSExitSlip.Application/Commands/CommandDto/ExitSlipDto/CreateExitSlipDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMSExitSlip.Application.Commands.CommandDto.ExitSlipDto
+{
+    internal class CreateExitSlipDto
+    {
+    }
+}
