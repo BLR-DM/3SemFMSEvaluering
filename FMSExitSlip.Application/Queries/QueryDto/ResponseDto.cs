f7b54c6e23504f73a03d527e13609916364be817
Bilal Kinali <biki59986@edu.ucl.dk>
2024-11-27 23:53:41 +0100

    added queries

---
+++ FMSExitSlip.Application/Queries/QueryDto/ResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FMSExitSlip.Application.Queries.QueryDto;
+
+public record ResponseDto
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string AppUserId { get; set; }
+}
