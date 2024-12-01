35d18674065b34b826d1bb71991ba70876296f1f
ZmolZito <maclucas07@hotmail.com>
2024-12-01 02:49:32 +0100

    Tilføjet getTeacher fra DataServer

---
+++ FMSDataServer.Api/ModelDto/TeacherDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FMSDataServer.Api.ModelDto
+{
+    public record TeacherDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string  Email { get; set; }
+        public List<TeacherSubjectDto> TeacherSubjects { get; set; }
+        public string AppUserId { get; set; }
+        
+    }
+}
