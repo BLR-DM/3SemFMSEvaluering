23c710d4ea780243822a54325147f380aa5293e6
skovbov <skovb@DESKTOP-TKGR0FM>
2024-12-03 00:47:24 +0100

    Added query for exitslip

---
+++ FMSExitSlip.Application/Queries/Interfaces/IExitSlipQuery.cs
@@ -0,0 +1,8 @@
+﻿using FMSExitSlip.Application.Queries.QueryDto;
+
+namespace FMSExitSlip.Application.Queries.Interfaces;
+
+public interface IExitSlipQuery
+{
+    Task<ExitSlipDto> GetExitSlipAsync(int exitSlipId, string appUserId, string role);
+}
--- FMSExitSlip.Application/Queries/QueryDto/QuestionDto.cs
+++ FMSExitSlip.Application/Queries/QueryDto/QuestionDto.cs
@@ -1,9 +1,10 @@
 ﻿namespace FMSExitSlip.Application.Queries.QueryDto;
 
-public record ResponseDto
+public record QuestionDto
 {
     public int Id { get; set; }
     public byte[] RowVersion { get; set; }
     public string Text { get; set; }
     public string AppUserId { get; set; }
+    public IEnumerable<ResponseDto> Responses { get; set; }
 }
