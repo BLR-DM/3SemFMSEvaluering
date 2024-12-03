ffdd0effcb2b76d3d3c7f39cec3faf27f30515d9
Bilal Kinali <biki59986@edu.ucl.dk>
2024-12-03 20:53:58 +0100

    Reponse UnitTest done

--- FMSExitSlip.Domain.Test/ReponseTests.cs
+++ FMSExitSlip.Domain.Test/ReponseTests.cs
@@ -1,4 +1,5 @@
-﻿using FMSExitSlip.Domain.Test.Fakes;
+﻿using FMSExitSlip.Domain.Entities;
+using FMSExitSlip.Domain.Test.Fakes;
 
 namespace FMSExitSlip.Domain.Test;
 
@@ -24,5 +25,85 @@ public class ReponseTests
 
         // Act & Assert
         Assert.Throws<ArgumentException>(() => sut.AssureUserIsCreator(appUserId));
+    }
+
+    [Theory]
+    [MemberData(nameof(ResponseTestWithOnlyOnePrQuestion))]
+    public void Given_Only_One_Response_For_Question__Then_Dont_Throw(Response response, IEnumerable<Response> responses)
+    {
+        // Arrange
+        var sut = new FakeResponse(response.Text, response.AppUserId);
+
+        // Act & Assert
+        sut.AssureOnlyOneResponsePrQuestion(responses);
+    }
+
+    [Theory]
+    [MemberData(nameof(ResponseWithMoreThanOnePrQuestion))]
+    public void Given_More_Than_One_Response_Pr_Question__Then_Throw(Response response, IEnumerable<Response> responses)
+    {
+        // Arrange
+        var sut = new FakeResponse(response.Text, response.AppUserId);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => sut.AssureOnlyOneResponsePrQuestion(responses));
+    }
+
+
+
+    public static IEnumerable<object[]> ResponseTestWithOnlyOnePrQuestion()
+    {
+        var responses = CreateResponses();
+        yield return
+        [
+            new FakeResponse("text","4"),
+            responses
+        ];
+
+        yield return
+        [
+            new FakeResponse("text","5"),
+            responses
+        ];
+
+        yield return
+        [
+            new FakeResponse("text","6"),
+            responses
+        ];
+    }
+
+    public static IEnumerable<object[]> ResponseWithMoreThanOnePrQuestion()
+    {
+        var responses = CreateResponses();
+
+        yield return
+        [
+            new FakeResponse("text","1"),
+            responses
+        ];
+
+        yield return
+        [
+            new FakeResponse("text","2"),
+            responses
+        ];
+
+        yield return
+        [
+            new FakeResponse("text","3"),
+            responses
+        ];
+    }
+
+    public static List<Response> CreateResponses()
+    {
+        var responses = new List<Response>(new[]
+        {
+        new FakeResponse("text","1"),
+        new FakeResponse("text","2"),
+        new FakeResponse("text","3")
+    });
+        return responses;
     }
 }
