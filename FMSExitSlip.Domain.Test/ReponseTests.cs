f82694db80688479ad75bbc76e9009937bbaaccf
Bilal Kinali <biki59986@edu.ucl.dk>
2024-12-03 20:29:02 +0100

    UnitTest on Response added, need the other method

---
+++ FMSExitSlip.Domain.Test/ReponseTests.cs
@@ -0,0 +1,28 @@
+﻿using FMSExitSlip.Domain.Test.Fakes;
+
+namespace FMSExitSlip.Domain.Test;
+
+public class ReponseTests
+{
+    [Theory]
+    [InlineData("1", "1")]
+    public void Given_User_Is_Same_As_Creator__Then_Dont_Throw(string appUserIdCreator, string appUserId)
+    {
+        // Arrange
+        var sut = new FakeResponse("text", appUserIdCreator);
+
+        // Act & Assert
+        sut.AssureUserIsCreator(appUserId);
+    }
+
+    [Theory]
+    [InlineData("1", "2")]
+    public void Given_User_Is_Not_Same_As_Creator__Then_Throw(string appUserIdCreator, string appUserId)
+    {
+        // Arrange
+        var sut = new FakeResponse("text", appUserIdCreator);
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => sut.AssureUserIsCreator(appUserId));
+    }
+}
